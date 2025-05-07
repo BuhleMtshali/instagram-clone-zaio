2208df328e80dbfdd31c3509428f7798d43ac7a2
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-07 22:18:41 +0200

    updated the firebase file

---
+++ instagram-clone/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4gZqO1ZmIfNQpeiI-Rm5z7b5rU_lHgPI",
+    authDomain: "instagram-clone-zaio.firebaseapp.com",
+    projectId: "instagram-clone-zaio",
+    storageBucket: "instagram-clone-zaio.firebasestorage.app",
+    messagingSenderId: "621090973907",
+    appId: "1:621090973907:web:e42da5e035143d0e154c26",
+    measurementId: "G-BYF2DC34JL"
+  };
+
+const app = initializeApp(firebaseConfig);
